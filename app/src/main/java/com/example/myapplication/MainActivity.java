34bd060353b121ad310f59963aea719314c78f4d
田中大誓 <2020m062@kuas.ac.jp>
2020-06-04 22:40:50 +0900

    implemented OnClickListener Set margins for LinearLayout in MainActivity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,34 +3,58 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mainButton1 = findViewById(R.id.MainButton1);
+        mainButton1.setOnClickListener(this);
+
+        TextView mainButton2 = findViewById(R.id.mainButton2);
+        mainButton2.setOnClickListener(this);
+
+        TextView mainButton3 = findViewById(R.id.mainButton3);
+        mainButton3.setOnClickListener(this);
+
+        TextView mainButton4 = findViewById(R.id.mainButton4);
+        mainButton4.setOnClickListener(this);
     }
 
-    public void goToLogActivity(View view) {
-        Intent toLog = new Intent(this, LogActivity.class);
-        startActivity(toLog);
-    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.MainButton1:
+                // If MainButton1 is clicked, do something
+                Intent toLog = new Intent(this, LogActivity.class);
+                startActivity(toLog);
+                break;
+            case R.id.mainButton2:
+                // If MainButton2 is clicked, do something
+                Intent toNormal = new Intent(this, NormalActivity.class);
+                startActivity(toNormal);
+                break;
+            case R.id.mainButton3:
+                    // If MainButton3 is clicked, do something
+                Intent toMechanism = new Intent(this, MechanismActivity.class);
+                startActivity(toMechanism);
+                    break;
+            case R.id.mainButton4:
+                // If MainButton4 is clicked, do something
+                Intent openFeverLink = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.healthline.com/health/how-to-break-a-fever"));
+                //Verify that the intent openFeverLink will resolve properly
+                if(openFeverLink.resolveActivity(getPackageManager()) != null){
+                    startActivity(openFeverLink);
+                }
 
-    public void goToNormalActivitiy(View view) {
-        Intent toNormal = new Intent( this, NormalActivity.class );
-        startActivity(toNormal);
-    }
-
-    public void goToMechanismActivity(View view) {
-        Intent toMechanism = new Intent(this, MechanismActivity.class);
-        startActivity(toMechanism);
-    }
-
-    public void goToHandling_Fever(View view) {
-        Intent toHandring = new Intent(this, Hanling_Fever.class);
-        startActivity(toHandring);
+                break;
+        }
     }
 }
