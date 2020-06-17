f49163a81f32cc8f27a9cde67aadf4e83e43a80d
田中大誓 <2020m062@kuas.ac.jp>
2020-06-17 23:56:25 +0900

    Homework 6.1: styled up LogActivity implemented data transfer using Intent in LogActivity

--- app/src/main/java/com/example/myapplication/LogActivity.java
+++ app/src/main/java/com/example/myapplication/LogActivity.java
@@ -1,14 +1,29 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LogActivity extends AppCompatActivity {
+
+    EditText userInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+        userInput = (EditText)findViewById(R.id.LogInputData);
+    }
+
+    public void confirmBT(View view) {
+        Intent toConfirm = new Intent(this, ConfirmActivity.class);
+        toConfirm.putExtra("BT_data", userInput.getText().toString());
+        startActivity(toConfirm);
     }
 }
--- app/src/main/java/com/example/myapplication/ReminderBroadcastReceiver.java
+++ app/src/main/java/com/example/myapplication/ReminderBroadcastReceiver.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -14,11 +15,18 @@ public class ReminderBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
+        Intent notificationIntent = new Intent(context, LogActivity.class);//Log activity screen
+        PendingIntent contentIntent = PendingIntent.getActivity(context,0,notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "BT_Tracker_Channel")
                 .setSmallIcon(R.mipmap.ic_launcher_round)
                 .setContentTitle("Notification from BT Tracker")
                 .setContentText("Please log your body temperature now")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true)
+                ;
                 NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
                 notificationManager.notify(200, builder.build());
     }
